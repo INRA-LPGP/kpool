fbeadcc67592a4e129249dc61ab3da7336ed473d
Romain Feron <romainferon91@gmail.com>
2018-06-26 14:39:03 +0200

    Implemented merge function

--- src/merge.cpp
+++ src/merge.cpp
@@ -1,6 +1,97 @@
 #include "merge.h"
 
-merge::merge()
-{
 
+void merge(Parameters& parameters) {
+
+    // Open input files
+    std::ifstream male_table_file, female_table_file;
+    std::string par = "male_table";
+    male_table_file.open(parameters.get_value_from_name<std::string>(par));
+    par = "female_table";
+    female_table_file.open(parameters.get_value_from_name<std::string>(par));
+
+    std::ofstream output_file;
+    par = "output_file";
+    output_file.open(parameters.get_value_from_name<std::string>(par));
+
+    std::vector<std::string> index_files_names {"AA", "AT", "AG", "AC",
+                                                "TA", "TT", "TG", "TC",
+                                                "CA", "CT", "CG", "CC",
+                                                "GA", "GT", "GG", "GC"};
+
+    std::cout << "# Indexing kmer tables ..." << std::endl;
+    std::cout << " - Indexing male table ..." << std::endl;
+    index_file(male_table_file, index_files_names, "_m");
+    std::cout << " - Indexing female table ..." << std::endl;
+    index_file(female_table_file, index_files_names, "_f");
+
+    uint kmer_count = 0;
+
+    std::cout << "# Merging indexed files ..." << std::endl;
+    for (auto file_name: index_files_names) {
+        process_batch(file_name, output_file, kmer_count);
+        remove((file_name + "_m" + ".kpool.tmp").c_str());
+        remove((file_name + "_f" + ".kpool.tmp").c_str());
+    }
+
+}
+
+
+
+void index_file(std::ifstream& input_file, std::vector<std::string>& index_files_names, std::string suffix) {
+
+    std::unordered_map<std::string, std::ofstream> index_files;
+
+    for (auto file_name: index_files_names) index_files[file_name].open(file_name + suffix + ".kpool.tmp");
+
+    std::string line;
+    uint lines = 0;
+
+    while (std::getline(input_file, line)) {
+
+        if (lines % 25000000 == 0) std::cout << "    -> Processed " << lines % 1000000 << " M. lines." << std::endl;
+        ++line;
+        index_files[line.substr(0, 2)] << line << "\n";
+    }
+
+    for (auto file_name: index_files_names) index_files[file_name].close();
+}
+
+
+
+void process_batch(std::string& file_name, std::ofstream& output_file, uint& kmer_count) {
+
+    std::unordered_map<std::string, std::pair<uint32_t, uint32_t>> table;
+
+    std::ifstream male_file, female_file;
+    male_file.open(file_name + "_m" + ".kpool.tmp");
+    female_file.open(file_name + "_f" + ".kpool.tmp");
+
+    std::string line;
+    std::vector<std::string> fields;
+
+    std::cout << " - Processing batch \"" << file_name << "\" ..." << std::endl;
+    std::cout << "    -> Processing male file ..." << std::endl;
+
+    while(std::getline(male_file, line)) {
+        fields = split(line, " ");
+        table[fields[0]].first = std::stoi(fields[1]);
+    }
+
+    std::cout << "    -> Processing female file ..." << std::endl;
+
+    while(std::getline(female_file, line)) {
+        fields = split(line, " ");
+        table[fields[0]].second = std::stoi(fields[1]);
+    }
+
+    std::cout << "    -> Found " << table.size() << " total kmers in this batch." << std::endl;
+
+    kmer_count += table.size();
+
+    std::cout << "    -> Writing to output file ... " << std::endl;
+
+    for (auto kmer: table) {
+        output_file << kmer.first << "\t" << kmer.second.first << "\t" << kmer.second.second << "\n";
+    }
 }
--- src/merge.h
+++ src/merge.h
@@ -1,11 +1,10 @@
-#ifndef MERGE_H
-#define MERGE_H
+#pragma once
+#include "unordered_map"
+#include "parameters.h"
+#include "utils.h"
 
+void merge(Parameters& parameters);
 
-class merge
-{
-    public:
-        merge();
-};
+void index_file(std::ifstream& input_file, std::vector<std::string>& index_files_names, std::string suffix);
 
-#endif // MERGE_H
+void process_batch(std::string& file_name, std::ofstream& output_file, uint& kmer_count);
